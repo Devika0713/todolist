ca086b0017f456fbba8634d5c9612603aaea781e
Devika0713 <devikauh620@gmail.com>
2021-09-28 18:53:11 +0530

    add gitignore,procfile and update

--- app.js
+++ app.js
@@ -109,7 +109,11 @@ app.post("/delete",(req,res)=>{
 //   res.redirect("/work");
 // });
 
+let port = process.env.PORT;
+if(port == null || port ==""){
+  port = 3000;
+}
 
-app.listen(3000, () => {
+app.listen(port , () => {
   console.log("server is running");
 });
